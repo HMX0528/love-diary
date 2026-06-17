a90b9c0a5cda0d771856c58a774a8a012d7b20ec
HMX0528 <3474854757@qq.com>
2026-06-17 20:25:27 +0800

    fix

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
+import http from "http";
+import fs from "fs";
+import path from "path";
+
 const PORT = process.env.PORT || 3000;
-const DIST = path.join(__dirname, "dist");
+const DIST = path.join(path.dirname(new URL(import.meta.url).pathname), "dist");
 
 const MIME = {
   ".html": "text/html; charset=utf-8",
   ".js": "application/javascript; charset=utf-8",
   ".css": "text/css; charset=utf-8",
   ".svg": "image/svg+xml",
-  ".json": "application/json",
 };
 
 http.createServer((req, res) => {
   let url = req.url.split("?")[0];
   if (url === "/") url = "/index.html";
-  const filePath = path.join(DIST, url);
-  const ext = path.extname(filePath);
-  fs.readFile(filePath, (err, data) => {
+  const ext = path.extname(url);
+
+  fs.readFile(path.join(DIST, url), (err, data) => {
     if (err) {
       fs.readFile(path.join(DIST, "index.html"), (err2, data2) => {
         if (err2) { res.writeHead(500); res.end("Error"); return; }
@@ -29,6 +29,4 @@ http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": MIME[ext] || "application/octet-stream" });
     res.end(data);
   });
-}).listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port " + PORT);
-});
+}).listen(PORT, "0.0.0.0", () => console.log("Running on port " + PORT));
